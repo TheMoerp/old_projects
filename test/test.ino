c3b0594b296dc0ffa494a241c8ba31aae930be77
matthias135 <matthias@hottgenroth.net>
2017-06-28 09:25:41 +0100

    Dim Funktion fertig

--- test/test.ino
+++ test/test.ino
@@ -1,39 +1,61 @@
 
   int lightPin = 2;
-  char a[5];
-  byte cI = 0;
+  char strInput[5];
+  byte inputCtr = 0;
+  char *strValPtr = 0;
+  int preVal = 0;
+  int val = 0;
   
 void setup() {
   Serial.begin(74880);
-  //pinMode(lightPin, OUTPUT);
-  Serial.print("Gebe die Value(0-1023)ein: ");
-  Serial.print(Serial.available()); 
+  pinMode(lightPin, OUTPUT);
+  analogWrite(lightPin, val);
+  Serial.println(" ");
+  Serial.println(" ");
+  Serial.println("Gebe eine Zahl zwischen 1 und 1023 ein:"); 
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    delay(10);
-    Serial.println(Input());
+  delay(10);
+  if(Serial.available() < 5){
+    if(Serial.available() > 0){
+      delay(10);
+      strValPtr = Input();
+      preVal = atoi(strValPtr);
+      if(preVal >0, preVal <=1023){
+        val = preVal;
+        Serial.println(val);
+        Serial.println("Zahl wurde als Value uebernommen.");
+      }
+      else{
+        Serial.println("Die Zahl muss zwischen 1 und 1023 liegen!");
+      }
+    }
   }
-  //analogWrite(lightPin, );
+  else{
+      Serial.println("Die Zahl muss zwischen 1 und 1023 liegen!"); 
+  }
+  while (Serial.read() >=0);
+  analogWrite(lightPin, val);
 }
 
 char * Input(){
+  inputCtr=0;
   for(int i = 0; i<=4; i++){
-          a[i]='\0';
-          cI=0; 
+          strInput[i]='\0';
   }
-  while(Serial.available() > 0){
-      char t = Serial.read();
-      a[cI]= t;
-      cI++;
-      if(cI>=4){
-        a[4]='\0';
-        return a;
-        }
+  while(Serial.available() > 0, Serial.available() < 5){
+      char iChar = Serial.read();
+      strInput[inputCtr]= iChar;
+      inputCtr++;
+      if(inputCtr>=4){
+        strInput[4]='\0';
+        return strInput;
       }
-    
-    a[cI]='\0';
-  return a;
+  }
+  if(Serial.available() < 5){  
+    strInput[inputCtr]='\0';
+    return strInput;
+  }
 }
 
