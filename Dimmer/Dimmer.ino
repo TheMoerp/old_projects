63107e5274be446567cf815bb7ffe88ec9dd23cd
matthias135 <matthias@hottgenroth.net>
2017-06-29 08:09:18 +0100

    Dimmer+Webserver

--- Dimmer/Dimmer.ino
+++ Dimmer/Dimmer.ino
@@ -1,15 +1,23 @@
 
+  #include <ESP8266WebServer.h>
+  #include <ESP8266WiFi.h>
   int lightPin = 2;
   char strInput[5];
   byte inputCtr = 0;
   char *strValPtr = 0;
   int preVal = 0;
   int val = 0;
+  char ssid[] = "WLan-KI-Pro";
+  char pass[] = "sVAPHCmo";
+  int conCtr = 0;
+  ESP8266WebServer server(80);
   
 void setup() {
   Serial.begin(74880);
   pinMode(lightPin, OUTPUT);
   analogWrite(lightPin, val);
+  wlanCon();
+  webServerInit();
   Serial.println(" ");
   Serial.println(" ");
   Serial.println("Gebe eine Zahl zwischen 1 und 1023 ein:"); 
@@ -37,6 +45,7 @@ void loop() {
   }
   while (Serial.read() >=0);
   analogWrite(lightPin, val);
+  server.handleClient();
 }
 
 char * Input(){
@@ -59,3 +68,37 @@ char * Input(){
   }
 }
 
+void wlanCon(){
+  Serial.println("Verbindungsversuch ");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  conCtr = 0;
+  while (WiFi.status() != WL_CONNECTED && conCtr <=20) {
+    delay(500);
+    Serial.print(".");
+    conCtr++;
+  }
+  Serial.println("");
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Verbindung zum WLAN konnte nicht hergestellt werden");
+  }
+  else {
+    Serial.println("Verbindung zum WLAN wurde hergestellt");
+    IPAddress ip = WiFi.localIP();
+    Serial.print("Die Ip-Addresse lautet: ");
+    Serial.println(ip);
+  }
+}
+
+void webServerInit(){
+  server.on("/", [](){
+    server.send(200, "text/html", "Hallo");
+  });
+  server.onNotFound([](){
+    server.send(200, "text/html", "404/Error");
+  });
+  server.begin();
+  Serial.println("Webserver ist online");
+ 
+}
